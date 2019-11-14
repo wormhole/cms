9f9470bc8fc7c240044f73c42bdaeccff20e8542
wormhole <363408268@qq.com>
2019-11-14 19:23:04 +0800

    添加redis和线程池配置类

---
+++ src/main/java/net/stackoverflow/cms/config/ThreadPoolConfiguration.java
@@ -0,0 +1,31 @@
+package net.stackoverflow.cms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * 线程池配置类
+ *
+ * @author 凉衫薄
+ */
+@Configuration
+public class ThreadPoolConfiguration implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        //定义线程池
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        //核心线程数
+        taskExecutor.setCorePoolSize(10);
+        //线程池最大线程数
+        taskExecutor.setMaxPoolSize(30);
+        //线程队列最大线程数
+        taskExecutor.setQueueCapacity(2000);
+        //初始化
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
