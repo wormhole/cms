036204c716346bf008eeedb71bba746deae18e0a
wormhole <363408268@qq.com>
2019-11-14 19:13:29 +0800

    添加应用配置

--- src/main/java/net/stackoverflow/cms/CmsApplication.java
+++ src/main/java/net/stackoverflow/cms/CmsApplication.java
@@ -1,9 +1,19 @@
 package net.stackoverflow.cms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"net.stackoverflow.cms.dao.*"}, sqlSessionFactoryRef = "sqlSessionFactory")
+@EnableCaching
+@EnableTransactionManagement
+@EnableScheduling
+@EnableAsync
 public class CmsApplication {
 
     public static void main(String[] args) {
