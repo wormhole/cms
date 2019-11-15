ae33bdd1e85c4c4a7954f37efc2e4fe097414722
wormhole <363408268@qq.com>
2019-11-15 12:17:20 +0800

    排序参数

--- src/main/java/net/stackoverflow/cms/common/Page.java
+++ src/main/java/net/stackoverflow/cms/common/Page.java
@@ -19,18 +19,28 @@ public class Page {
     private Integer page;
     private Integer limit;
     private Integer offset;
+    private String order;
+    private String sort;
     private Map<String, Object> searchParam;
 
-    public Page(Integer page, Integer offset) {
+    public Page(Integer page, Integer limit) {
         this.page = page;
-        this.offset = offset;
+        this.limit = limit;
         this.offset = (page - 1) * limit;
     }
 
-    public Page(Integer page, Integer offset, Map<String, Object> searchParam) {
+    public Page(Integer page, Integer limit, String order, String sort) {
         this.page = page;
-        this.offset = offset;
-        this.searchParam = searchParam;
+        this.limit = limit;
         this.offset = (page - 1) * limit;
+        this.order = order;
+        this.sort = sort;
+    }
+
+    public Page(Integer page, Integer limit, Map<String, Object> searchParam) {
+        this.page = page;
+        this.limit = limit;
+        this.offset = (page - 1) * limit;
+        this.searchParam = searchParam;
     }
 }
