2e5d27dce2e63a3ab324577c8f6280cbadf941cf
wormhole <363408268@qq.com>
2019-12-11 19:31:14 +0800

    登录js

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,7 +1,4 @@
-layui.use(['form', 'jquery'], function () {
-    var $ = layui.$;
-    var form = layui.form;
-
+$(function () {
     $('#verify-img').click(function () {
         $(this).attr('src', $(this).attr('src') + '?' + Math.random());
     });
