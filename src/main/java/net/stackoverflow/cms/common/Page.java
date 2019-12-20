f36b566e1d6ca79bba52ec8ec5640a96023ab50f
wormhole <363408268@qq.com>
2019-12-20 19:26:55 +0800

    分页参数，条件查询，模糊查询

--- src/main/java/net/stackoverflow/cms/common/Page.java
+++ src/main/java/net/stackoverflow/cms/common/Page.java
@@ -23,7 +23,7 @@ public class Page {
     private String order = "id";
     private String sort = "asc";
     private Map<String, Object> searchMap;
-    private Map<String, Object> likeMap;
+    private String key;
 
     public Page(Integer page, Integer limit) {
         this.page = page;
@@ -31,7 +31,7 @@ public class Page {
         this.offset = (page - 1) * limit;
     }
 
-    public Page(Integer page, Integer limit, String order, String sort) {
+    public Page(Integer page, Integer limit, String order, String sort, Map<String, Object> searchMap, String key) {
         this.page = page;
         this.limit = limit;
         this.offset = (page - 1) * limit;
@@ -41,13 +41,9 @@ public class Page {
         if (StringUtils.isNotBlank(sort)) {
             this.sort = sort;
         }
-    }
-
-    public Page(Integer page, Integer limit, Map<String, Object> searchMap, Map<String, Object> likeMap) {
-        this.page = page;
-        this.limit = limit;
-        this.offset = (page - 1) * limit;
         this.searchMap = searchMap;
-        this.likeMap = likeMap;
+        if (StringUtils.isNotBlank(key)) {
+            this.key = key;
+        }
     }
 }
