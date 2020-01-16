c37aa49b1ce233b8fe945d79bc892db0d57a7439
wormhole <363408268@qq.com>
2020-01-16 18:29:26 +0800

    去除未使用的导入

---
+++ src/main/java/net/stackoverflow/cms/web/controller/personal/PersonalController.java
@@ -0,0 +1,8 @@
+package net.stackoverflow.cms.web.controller.personal;
+
+import net.stackoverflow.cms.common.BaseController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonalController extends BaseController {
+}
