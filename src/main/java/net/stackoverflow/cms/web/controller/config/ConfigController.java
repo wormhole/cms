6b5e9252c2b285cc9c7be614955148fd8e395b48
wormhole <363408268@qq.com>
2020-01-19 14:28:40 +0800

    配置信息更新

--- src/main/java/net/stackoverflow/cms/web/controller/config/ConfigController.java
+++ src/main/java/net/stackoverflow/cms/web/controller/config/ConfigController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -59,7 +60,7 @@ public class ConfigController extends BaseController {
      * @param configVOs
      * @return
      */
-    @RequestMapping(value = "/update")
+    @PutMapping(value = "/update")
     public ResponseEntity update(List<ConfigVO> configVOs) {
         Result result = new Result();
         try {
