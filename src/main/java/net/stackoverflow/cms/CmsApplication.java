a93c0cab87ec805731f017012c28863bee039b6e
wormhole <363408268@qq.com>
2020-05-26 19:32:25 +0800

    去除enablecache

--- src/main/java/net/stackoverflow/cms/CmsApplication.java
+++ src/main/java/net/stackoverflow/cms/CmsApplication.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.jdbc.ScriptRunner;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -17,7 +16,6 @@ import java.util.Properties;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"net.stackoverflow.cms.dao"}, sqlSessionFactoryRef = "sqlSessionFactory")
-@EnableCaching
 @EnableTransactionManagement
 @EnableScheduling
 @EnableAsync
